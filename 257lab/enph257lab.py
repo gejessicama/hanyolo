7fc72e23b118a9ae5171bb1f949ba1961c2ef00e
gejessicama <gejessicama@gmail.com>
2018-06-01 14:22:13 -0700

    updated for multiple pins

--- 257lab/enph257lab.py
+++ 257lab/enph257lab.py
@@ -1,17 +1,19 @@
 import serial
 import numpy as np
 import matplotlib as mtb
+import matplotlib.pyplot as plt
 import pylab as pyl
+import time
 
 # initialize serial 
-port_name = "COM4"
+port_name = "COM5"
 baudrate = 9600
 ser = serial.Serial(port_name,baudrate)
 
 # creates file with dated name
 timestr = time.strftime("%Y%m%d-%H%M")
 datafile = open(timestr + ".csv", "w+")
-datafile.write("A0,A1,A2,A3,A4,time\n")
+datafile.write("A1,A2,A3,A4,A5,time\n")
 
 # read data into an array 
 def read_data():
@@ -21,14 +23,18 @@ def read_data():
 	datafile.write(data)
 	return list(map(int, data.split(",")))
 
-msmt = read_data()
-datafile.write(msmt)
+
+while True:
+	read_data()
+'''
+temp = msmt[:6]
 X = [1,2,3,4,5]
 
 plt.ion()
-graph = plt.plot(X,msmt)[0]
+graph = plt.plot(X,temp)[0]
 
 while True:
-    graph.set_ydata(msmt)
+    graph.set_ydata(temp)
     plt.draw()
     plt.pause(0.01)
+ '''
--- 257lab/enph257lab/enph257lab.ino
+++ 257lab/enph257lab/enph257lab.ino
@@ -1,17 +1,30 @@
 void setup() {
   Serial.begin(9600);
 }
-
 void loop() {
+
+  int pA1 = 0;
+  int pA2 = 0;
+  int pA3 = 0;
+  int pA4 = 0;
+  int pA5 = 0;
   
-  int pA0 = analogRead(A0);
-  int pA1 = analogRead(A1);
-  int pA2 = analogRead(A2);
-  int pA3 = analogRead(A3);
-  int pA4 = analogRead(A4);
+  for (int i=0; i<5; i++){
+     pA1 += analogRead(A1);
+     pA2 += analogRead(A2);
+     pA3 += analogRead(A3);
+     pA4 += analogRead(A4);
+     pA5 += analogRead(A5);
+    }
+ 
+  double t1 = 0.1*(pA1/1.024)-50;
+  double t2 = 0.1*(pA2/1.024)-50;
+  double t3 = 0.1*(pA3/1.024)-50;
+  double t4 = 0.1*(pA4/1.024)-50;
+  double t5 = 0.1*(pA5/1.024)-50;
   
   // Converts pin readings to string in csv format
-  String data = String(pA0) + ',' + String(pA1) +  ',' + String(pA2) +  ',' + String(pA3) + ',' + String(pA4);
+  String data = String(t1) +  ',' + String(t2) +  ',' + String(t3) + ',' + String(t4) + ',' + String(t5) + ',' + String(millis());
 
   // Print data to serial in string format, to be split in python
   Serial.println(data);
