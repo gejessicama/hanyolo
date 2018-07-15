ac5072ba2594f089f3d06e3da534d0a96560ae54
Maria Sottile <37maria.sottile@gmail.com>
2018-07-15 14:31:22 -0700

    maybe some stuff for testing the sonar

--- PIDTapeFollower/Tape_follower.ino
+++ PIDTapeFollower/Tape_follower.ino
@@ -9,6 +9,11 @@ Motion robot(54,0);
 
 void loop() {
     robot.followTape();
+    if(Serial1.available()){
+      char c = Serial1.read();
+      if (c == 's' || c == 'S' || c == 'c' || c == 'C')
+        delay (10000);
+    }
 }
 
 
