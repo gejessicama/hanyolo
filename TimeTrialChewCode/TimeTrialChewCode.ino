61a402392e76afcd70403e25665bdc33c74e36a8
Maria Sottile <37maria.sottile@gmail.com>
2018-07-27 08:13:53 -0700

    arduino code done

--- TimeTrialChewCode/TimeTrialChewCode.ino
+++ TimeTrialChewCode/TimeTrialChewCode.ino
@@ -10,31 +10,17 @@
 #define leftTriggerPin 6
 #define leftEchoPin  7
 
-#define bridgeDropPin 13
-
-#define fromSoloPin 2
 #define toSoloPin 0
-#define dropTheBridgePin 3
 #define pauseForStuffy 3000
 
-//const uint16_t objectLimit = 400;
 const uint16_t stuffyLimit = 60000;
 
-Servo bridge;
-volatile boolean firstBridgeDropped = false;
-
 boolean readInSonar(uint8_t, uint8_t, uint16_t);
-void dropBridge();
 
 void setup() {
-  Serial.begin(9600);
   pinMode(rightTriggerPin, OUTPUT);
   pinMode(leftTriggerPin, OUTPUT);
   pinMode(toSoloPin, OUTPUT);
-  bridge.attach(bridgeDropPin);
-  bridge.write(0);
-  attachInterrupt(digitalPinToInterrupt(dropTheBridgePin), dropBridge, RISING);
-  Serial.println("setup");
 }
 
 void loop() {
@@ -57,17 +43,3 @@ boolean readInSonar(uint8_t trig, uint8_t echo) {
   digitalWrite(trig, LOW);
   return (pulseIn(echo, HIGH) > stuffyLimit);
 }
-
-void dropBridge() {
-  if(!firstBridgeDropped){
-    
-    bridge.write(120);
-    firstBridgeDropped = true;
-    Serial.println("drop first bridge");
-  }else{
-    bridge.write(180);
-    Serial.println("drop second bridge");
-  }
-}
-
-
